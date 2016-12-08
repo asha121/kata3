1f5027b15b587ca76245789e026a8367c4d2da08
Test developer <test@testmail.com>
2016-12-08 20:44:34 +0530

    GREEN >> PASSING TEST CASE

--- kata-num-word/src/main/java/com/convert/example/NumberToWord.java
+++ kata-num-word/src/main/java/com/convert/example/NumberToWord.java
@@ -2,12 +2,12 @@ package com.convert.example;
 
 public class NumberToWord {
 
-	public String convert(int i) {
-		String[] words = {"ONE", "TWO" };
-		if (i>2) {
+	public String convert(int i){
+		String[] ones = {"0 NOT A POSTIVE INTEGER","ONE", "TWO" };
+		if (i>10) {
 			return "TWENTY THREE";
 		}
-		return words[--i];
+		return ones[i];
 	}
 
 }
