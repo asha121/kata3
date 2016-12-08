3a0d7493c98c68ff1151717f78b3edf6136298b8
Test developer <test@testmail.com>
2016-12-08 21:51:30 +0530

    REFACTOR >>>code

--- kata-num-word/src/main/java/com/convert/example/NumberToWord.java
+++ kata-num-word/src/main/java/com/convert/example/NumberToWord.java
@@ -4,10 +4,11 @@ public class NumberToWord {
 
 	public String convert(int i) {
 		String word = "";
-		String[] ones = { "0 NOT A POSTIVE INTEGER", "ONE ", "TWO ", "THREE ", "FOUR ", "FIVE ", "SIX ", "SEVEN ", "EIGHT ",
-				"NINE" };
+		String[] ones = { "0 NOT A POSTIVE INTEGER", "ONE ", "TWO ", "THREE ", "FOUR ", "FIVE ", "SIX ", "SEVEN ",
+				"EIGHT ", "NINE" };
 		String[] teen = { "TEN ", "ELEVEN ", "TWELVE ", "THIRTEEN ", "FOURTEEN ", "FIFTEEN ", "SIXTEEN ", "SEVENTEEN ",
 				"EIGHTEEN ", "NINETEEN" };
+		String[] enty = { "TWENTY " };
 		if (i < 10)
 
 		{
@@ -16,9 +17,9 @@ public class NumberToWord {
 		} else if (i < 20) {
 			word = teen[i - 10];
 		} else if (i == 20) {
-			word = "TWENTY ";
+			word = enty[0];
 		} else if (i % 20 > 0) {
-			word = "TWENTY "+convert(i % 20);
+			word = enty[0] + convert(i % 20);
 		}
 		return word;
 	}
