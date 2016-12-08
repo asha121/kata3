ee228e3498a65cb8136321b2d10f6c1f4f7d46b7
Test developer <test@testmail.com>
2016-12-08 21:05:51 +0530

    RED >>   test for ELEVEN

--- kata-num-word/src/test/java/com/convert/example/NumberShouldBeConvertedToWord.java
+++ kata-num-word/src/test/java/com/convert/example/NumberShouldBeConvertedToWord.java
@@ -10,17 +10,24 @@ public class NumberShouldBeConvertedToWord {
 	public void check_1_converted_one() throws Exception {
 		assertEquals("ONE", new NumberToWord().convert(1));
 	}
+
 	@Test
 	public void check_23_converted_twenty_three() throws Exception {
 		assertEquals("TWENTY THREE", new NumberToWord().convert(23));
 	}
+
 	@Test
 	public void check_2_convert_Two() throws Exception {
 		assertEquals("TWO", new NumberToWord().convert(2));
 	}
+
 	@Test
-	public void check_for_0_convert_Two() throws Exception { 
+	public void check_for_0_convert_Two() throws Exception {
 		assertEquals("0 NOT A POSTIVE INTEGER", new NumberToWord().convert(0));
 	}
-	
+	@Test
+	public void check_for_11_convert_eleven() throws Exception {
+		assertEquals("ELEVEN", new NumberToWord().convert(11));
+	}
+
 }
