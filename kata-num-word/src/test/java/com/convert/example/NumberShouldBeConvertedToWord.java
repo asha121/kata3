63278343cd2c00e7f47cfe26e6af0c2a5078d3de
Test developer <test@testmail.com>
2016-12-08 19:28:45 +0530

    RED >> FAILing

--- kata-num-word/src/test/java/com/convert/example/NumberShouldBeConvertedToWord.java
+++ kata-num-word/src/test/java/com/convert/example/NumberShouldBeConvertedToWord.java
@@ -10,4 +10,8 @@ public class NumberShouldBeConvertedToWord {
 	public void check_1_converted_one() {
 		assertEquals("ONE", new NumberToWord().convert(1));
 	}
+	@Test
+	public void check_23_converted_twenty_three() {
+		assertEquals("TWENTY THREE", new NumberToWord().convert(23));
+	}
 }
