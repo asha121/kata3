29b5da8d6aaf405a8aedd6105da71a42c5e0f7f0
Test developer <test@testmail.com>
2016-12-08 20:42:43 +0530

    RED >> BOUNDARY CASE FOR ZERO

--- kata-num-word/src/test/java/com/convert/example/NumberShouldBeConvertedToWord.java
+++ kata-num-word/src/test/java/com/convert/example/NumberShouldBeConvertedToWord.java
@@ -7,16 +7,20 @@ import org.junit.Test;
 public class NumberShouldBeConvertedToWord {
 
 	@Test
-	public void check_1_converted_one() {
+	public void check_1_converted_one() throws Exception {
 		assertEquals("ONE", new NumberToWord().convert(1));
 	}
 	@Test
-	public void check_23_converted_twenty_three() {
+	public void check_23_converted_twenty_three() throws Exception {
 		assertEquals("TWENTY THREE", new NumberToWord().convert(23));
 	}
 	@Test
-	public void check_2_convert_Two() {
+	public void check_2_convert_Two() throws Exception {
 		assertEquals("TWO", new NumberToWord().convert(2));
+	}
+	@Test
+	public void check_for_0_convert_Two() throws Exception { 
+		assertEquals("0 NOT A POSTIVE INTEGER", new NumberToWord().convert(0));
 	}
 	
 }
