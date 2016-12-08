a4ad993bf753571158de797448b5d1c8853086a4
Test developer <test@testmail.com>
2016-12-08 20:51:25 +0530

    REFACTOR > ONES DIGIT

--- kata-num-word/src/main/java/com/convert/example/NumberToWord.java
+++ kata-num-word/src/main/java/com/convert/example/NumberToWord.java
@@ -2,9 +2,12 @@ package com.convert.example;
 
 public class NumberToWord {
 
-	public String convert(int i){
-		String[] ones = {"0 NOT A POSTIVE INTEGER","ONE", "TWO" };
-		if (i>10) {
+	public String convert(int i) {
+		String[] ones = { "0 NOT A POSTIVE INTEGER", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT",
+				"NINE" };
+		if (i > 10)
+
+		{
 			return "TWENTY THREE";
 		}
 		return ones[i];
