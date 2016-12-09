776ad0b9e25867bb0b7c7bccd2f7073fa443937d
Test developer <test@testmail.com>
2016-12-09 11:30:11 +0530

    REFACTORING>>>

--- kata-num-word/src/main/java/com/convert/example/NumberToWord.java
+++ kata-num-word/src/main/java/com/convert/example/NumberToWord.java
@@ -27,63 +27,84 @@ public class NumberToWord {
 
 	private String digitToNumber(int number, String word, String[] ones, String[] enty, String[] maxs) {
 		char digit[] = (number + "").toCharArray();
-		String and = "";
-		int count = 0;
 		if (digit.length == 2) {
-			for (char charNum : digit) {
-				if (count == 0) {
-					word = enty[Integer.parseInt(Character.toString(charNum)) - 2];
-					count++;
-				} else if (Integer.parseInt(Character.toString(charNum)) != 0) {
-					word += ones[Integer.parseInt(Character.toString(charNum))];
-				}
-
-			}
+			word = twoDigitWord(digit, ones, enty);
 		} else if (digit.length == 3) {
-			for (char charNum : digit) {
-				if (charNum != '0') {
-					if (count == 0) {
-						word = ones[Integer.parseInt(Character.toString(charNum))] + maxs[0];
-						count++;
-					} else if (count == 1) {
-						word += "AND " + enty[Integer.parseInt(Character.toString(charNum)) - 2];
-						count++;
-					} else if (Integer.parseInt(Character.toString(charNum)) != 0) {
-						word += and + ones[Integer.parseInt(Character.toString(charNum))];
-					}
-				} else {
-					if (!and.equals("AND ")) {
-						and = "AND ";
-					}
-					count++;
-				}
-
-			}
+			word = threeDigitWord(digit, ones, enty, maxs);
 		} else if (digit.length == 4) {
-			for (char charNum : digit) {
-				if (charNum != '0') {
-					if (count == 0) {
-						word = ones[Integer.parseInt(Character.toString(charNum))] + maxs[1];
-						count++;
-					} else if (count == 1) {
-						word += ones[Integer.parseInt(Character.toString(charNum))] + maxs[0];
-						count++;
-					} else if (count == 2) {
-						and = "AND ";
-						word += and + enty[Integer.parseInt(Character.toString(charNum)) - 2];
-					} else if (Integer.parseInt(Character.toString(charNum)) != 0) {
-						word += and + ones[Integer.parseInt(Character.toString(charNum))];
-					}
-				} else {
-					if (!and.equals("AND ")) {
-						and = "AND ";
-					}
-					count++;
-				}
-
-			}
+			word = fourDigitFourWord(digit, ones, enty, maxs);
 		}
 
+		return word;
+	}
+
+	private String fourDigitFourWord(char[] digit, String[] ones, String[] enty, String[] maxs) {
+		String and = "";
+		int count = 0;
+		String word = "";
+		for (char charNum : digit) {
+			if (charNum != '0') {
+				if (count == 0) {
+					word = ones[Integer.parseInt(Character.toString(charNum))] + maxs[1];
+					count++;
+				} else if (count == 1) {
+					word += ones[Integer.parseInt(Character.toString(charNum))] + maxs[0];
+					count++;
+				} else if (count == 2) {
+					and = "AND ";
+					word += and + enty[Integer.parseInt(Character.toString(charNum)) - 2];
+				} else if (Integer.parseInt(Character.toString(charNum)) != 0) {
+					word += and + ones[Integer.parseInt(Character.toString(charNum))];
+				}
+			} else {
+				if (!and.equals("AND ")) {
+					and = "AND ";
+				}
+				count++;
+			}
+
+		}
+		return word;
+	}
+
+	private String threeDigitWord(char[] digit, String[] ones, String[] enty, String[] maxs) {
+		String and = "";
+		int count = 0;
+		String word = "";
+		for (char charNum : digit) {
+			if (charNum != '0') {
+				if (count == 0) {
+					word = ones[Integer.parseInt(Character.toString(charNum))] + maxs[0];
+					count++;
+				} else if (count == 1) {
+					word += "AND " + enty[Integer.parseInt(Character.toString(charNum)) - 2];
+					count++;
+				} else if (Integer.parseInt(Character.toString(charNum)) != 0) {
+					word += and + ones[Integer.parseInt(Character.toString(charNum))];
+				}
+			} else {
+				if (!and.equals("AND ")) {
+					and = "AND ";
+				}
+				count++;
+			}
+
+		}
+		return word;
+	}
+
+	private String twoDigitWord(char[] digit, String[] ones, String[] enty) {
+		int count = 0;
+		String word = "";
+		for (char charNum : digit) {
+			if (count == 0) {
+				word = enty[Integer.parseInt(Character.toString(charNum)) - 2];
+				count++;
+			} else if (Integer.parseInt(Character.toString(charNum)) != 0) {
+				word += ones[Integer.parseInt(Character.toString(charNum))];
+			}
+
+		}
 		return word;
 	}
 }
